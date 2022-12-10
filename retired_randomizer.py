71ca96fe33f69de4c81aeedaf661517413ffa26c
matteggers <94013259+matteggers@users.noreply.github.com>
2022-12-10 16:06:33 -0500

    accepted changes

--- retired_randomizer.py
+++ retired_randomizer.py
@@ -13,13 +13,6 @@ alphabet = "abcdefghijklmnopqrstuvwxyz"
 for i in range(len(alphabet)):
     addLetters = random.choice(alphabet)
     dataSet.append(addLetters)
-<<<<<<< HEAD:retired_randomizer.py
-   
-poppedSet = [] #Declared outside because I need it following the segAlpha function       
-   
-def segregateAlpha(dataSet):
-    soupAlphabet = []
-=======
     
   
 numList = []
@@ -28,7 +21,6 @@ evenList = []
 oddList = []
  
 def segAll(dataSet):
->>>>>>> db409ea4621f3926e4e7960fbd25d6533d3854df:randomizer.py
     for i in dataSet:
         if isinstance(i, int): #checks to see if i is an integer
             numList.append(i)
